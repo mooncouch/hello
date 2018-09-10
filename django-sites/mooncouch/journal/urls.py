69a9a061a8f7a5d22f8428b9add529086e0767cc
mooncouch <cottell.andrew@gmail.com>
2018-09-10 01:02:53 -0400

    Update urls.py

--- django-sites/mooncouch/journal/urls.py
+++ django-sites/mooncouch/journal/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /journal/
     path('', views.index, name='index'),
+    # ex: /journal/1/
+    path('<int:author_id>/', views.detail, name='author'),
+    # ex: /journal/1/entry/
+    path('<int:author_id>/entry/', views.entry, name='entry'),
+    # ex: /polls/5/vote/
+    path('<int:author_id>/summary/', views.summary, name='summary'),
 ]
