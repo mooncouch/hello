6d118e287c7bd8d5573e9086bc486a636108e947
mooncouch <cottell.andrew@gmail.com>
2018-09-13 16:57:38 -0400

    Update views.py

--- django-sites/mooncouch/journal/views.py
+++ django-sites/mooncouch/journal/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("You're at the journal index.")
 
+def detail(request, author_id):
+    return HttpResponse("you're looking at author %s." %author_id)
+
+def entry(request, author_id):
+    response = "You're looking at the entries of author %s."
+    return HttpResponse(response % author_id)
+
+def summary(request, author_id):
+    return HttpResponse("You're the summary of author %s." % author_id)
