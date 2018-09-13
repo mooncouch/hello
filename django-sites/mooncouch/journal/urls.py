3ff885174e1c44b657f159de4f3608eeb34f500f
mooncouch <cottell.andrew@gmail.com>
2018-09-13 17:00:47 -0400

    Update urls.py

--- django-sites/mooncouch/journal/urls.py
+++ django-sites/mooncouch/journal/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     # ex: /journal/1/
     path('<int:author_id>/', views.detail, name='author'),
     # ex: /journal/1/entry/
-    path('<int:author_id>/entry/', views.entry, name='entry'),
+    path('<int:author_id>/entry/', views.entry_sum, name='entry_sum'),
     # ex: /polls/5/vote/
     path('<int:author_id>/summary/', views.summary, name='summary'),
     #how to make a view that populates based off of entry_id
+    path('<int:author_id>/entry/<int:entry_id>', views.entry, name='entry'),
 ]
 
